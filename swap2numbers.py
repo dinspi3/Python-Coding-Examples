f43fad4c349921bd01c3535ed7888ef11f2fc907
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-06-07 16:36:07 +0300

    Add files via upload

---
+++ swap2numbers.py
@@ -0,0 +1,15 @@
+
+#n1=10
+#n2=20
+
+n1 = input("Enter first number : ")
+n2 = input("Enter second number : ")
+
+print("first number before swapping",n1)
+print("second number before swapping",n2)
+
+n1,n2=n2,n1
+
+print("first number after swapping",n1)
+print("second number after swapping",n2)
+
